9346a8f631a7e3728696dc13e81961a3da5056fb
Trey <104473511+Lemonns@users.noreply.github.com>
2022-09-27 05:07:19 -0500

    Added client termination

--- ppp.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.call("TASKKILL /F /IM RiotClientServices.exe")
-subprocess.call("TASKKILL /F /IM LeagueClient.exe")
-subprocess.call("TASKKILL /F /IM LeagueClientUx.exe")
-subprocess.call("TASKKILL /F /IM LeagueClientUxRender.exe")
