6acae5337818e28504bf0a7e855928cf6487aeca
Trey <104473511+Lemonns@users.noreply.github.com>
2022-10-11 22:44:25 -0500

    Update keyboard_sim.py

--- src/keyboard_sim.py
+++ src/keyboard_sim.py
@@ -1,4 +1,6 @@
-from subprocess import Popen, PIPE
+from subprocess import *
+import subprocess
+import psutil
 from pynput.keyboard import Key, Controller
 import time
 
@@ -7,9 +9,18 @@ class Commands():
     def __init__(self, file_location):
         self.keyboard = Controller()
         self.location = file_location
-        self.opener = Popen([self.location],stdin=PIPE,stdout=PIPE,stderr=PIPE, encoding="UTF8")
+        #self.open = Popen([self.location],stdin=PIPE,stdout=PIPE,stderr=PIPE, encoding="UTF8")
 
     def enter_information(self):
+        #If client already open, close it
+        if "RiotClientServices.exe" in (i.name() for i in psutil.process_iter()):
+            subprocess.call("TASKKILL /F /IM RiotClientServices.exe")
+            subprocess.call("TASKKILL /F /IM LeagueClient.exe")
+            subprocess.call("TASKKILL /F /IM LeagueClientUx.exe")
+            subprocess.call("TASKKILL /F /IM LeagueClientUxRender.exe")
+            time.sleep(2)
+            
+        Popen([self.location],stdin=PIPE,stdout=PIPE,stderr=PIPE, encoding="UTF8")   
         time.sleep(5)
         self.keyboard.type("Username")
         self.keyboard.press(Key.tab)
@@ -18,7 +29,3 @@ class Commands():
         self.keyboard.release(Key.tab)
         self.keyboard.press(Key.enter)
         self.keyboard.release(Key.enter)
-
-
-main_controller = Commands("D:\Riot Games\Riot Client\RiotClientServices.exe")
-main_controller.enter_information()
